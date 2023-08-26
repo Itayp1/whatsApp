b47c390e15b1a815ee53ba119f8f6b964aa27b1c
unknown <{ID}+{username}@users.noreply.github.com>
2023-08-26 22:39:11 +0300

    change chat version to 7

--- deploy.bat
+++ deploy.bat
@@ -1,9 +1,9 @@
-kubernates-yaml --env=qa --name=whatsapp-bot-k8s --operation=deployment --image=itayp/itayp/whatsapp-bot-k8s:6
+kubernates-yaml --env=qa --name=whatsapp-bot-k8s --operation=deployment --image=itayp/itayp/whatsapp-bot-k8s:7
 kubernates-yaml --env=qa --name=whatsapp-bot-k8s --domain=digital-cloud-services.com --operation=Ingress
 kubernates-yaml --env=qa --name=whatsapp-bot-k8s --operation=Service
 
-docker build -t itayp/whatsapp-bot-k8s:6 -t itayp/whatsapp-bot-k8s:latest  .
-docker push itayp/whatsapp-bot-k8s:6
+docker build -t itayp/whatsapp-bot-k8s:7 -t itayp/whatsapp-bot-k8s:latest  .
+docker push itayp/whatsapp-bot-k8s:7
 docker push itayp/whatsapp-bot-k8s:latest
 
  
